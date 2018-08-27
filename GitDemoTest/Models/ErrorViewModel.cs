4d16e9486d6351683d1b9bc4267155abcded1f88
446241967@qq.com <446241967@qq.com>
2018-08-27 11:06:38 +0800

    first Commit

---
+++ GitDemoTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitDemoTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
